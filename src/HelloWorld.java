2d1cfa29a4adf1044d7e813d2575cde7c423f04e
Vibin Daniel <vibindaniel@outlook.com>
2017-08-22 11:19:02 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by vibin on 8/22/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        for (int i=1; i<11; i++)
+            System.out.println(i);
+    }
+
+}
